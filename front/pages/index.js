7fcf28363e250061bbf6313a249015e7e787c0bb
jungwoole91 <jungwoolee91@gmail.com>
2020-09-10 01:43:48 +0900

    1-4) antd 적용하기

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,19 @@
 // 메인화면
 import Link from 'next/link';
+import AppLayout from "../components/AppLayout";
+import Head from 'next/head';
+
 
 const Home = () => {
   return (
     <>
-      <Link href="/about"><a>about</a></Link>
-      <div>Hello, next</div>
+      <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.6.3/antd.min.css"/>
+      </Head>
+      <AppLayout>
+        <Link href="/about"><a>about</a></Link>
+        <div>Hello, next</div>
+      </AppLayout>
     </>
   )
 };
