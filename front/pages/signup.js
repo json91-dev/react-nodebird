cf599d4edc4aefcea4080f6c4aad10520c30ba6e
jungwoole91 <jungwoolee91@gmail.com>
2020-09-14 01:02:20 +0900

    1-7) 회원가입 state와 custom hook

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -11,7 +11,22 @@ const Signup = () => {
   const [passwordCheck, setPasswordCheck] = useState('');
   const [term, setTerm] = useState(false);
 
-  const onSubmit = () => {};
+  const [passwordError, setPasswordError] = useState(false);
+  const [termError, setTermError] = useState(false);
+
+
+  const onSubmit = (e) => {
+    // e.preventDefault();
+
+    if (password !==  passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!term) {
+      return setTermError(true);
+    }
+
+    console.log({id, nick, password, passwordCheck, term});
+  };
 
   const onChangeId = (e) => {
     setId(e.target.value);
@@ -26,10 +41,14 @@ const Signup = () => {
   };
 
   const onChangePasswordChk = (e) => {
+    setPasswordError(e.target.value !== password);
     setPasswordCheck(e.target.value);
   };
 
-  const onChangeTerm = () => {}
+  const onChangeTerm = (e) => {
+    setTermError(false);
+    setTerm(e.target.checked);
+  };
 
   return (
     <>
@@ -37,7 +56,7 @@ const Signup = () => {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.6.3/antd.min.css"/>
       </Head>
       <AppLayout>
-        <Form onSubmit={onSubmit} style={{padding: 10}}>
+        <Form onFinish={onSubmit} style={{padding: 10}}>
           <div>
             <label htmlFor="user-id">아이디</label>
             <br/>
@@ -58,16 +77,16 @@ const Signup = () => {
             <label htmlFor="user-password-chk">비밀번호 체크</label>
             <br/>
             <Input name="user-password-check" value={passwordCheck} required onChange={onChangePasswordChk}/>
+            {passwordError && <div style={{color: 'red'}}>비밀번호가 일치하지 않습니다.</div>}
           </div>
           <div>
             <Checkbox name="user-term" onChange={onChangeTerm}>제로초 말을 잘 들을 것을 동의합니다.</Checkbox>
+            {termError && <div style={{color: 'red'}}>약관에 동의하셔야 합니다.</div>}
           </div>
-          <div>
+          <div style={{marginTop: 10}}>
             {/*파란색버튼*/}
             <Button type="primary" htmlType="submit">가입하기</Button>
           </div>
-
-
         </Form>
       </AppLayout>
     </>
