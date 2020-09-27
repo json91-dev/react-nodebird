49a560981ca11b55477eaf1f59ef33c7a3a5f5ba
jungwoole91 <jungwoolee91@gmail.com>
2020-09-27 22:36:46 +0900

    4-2,4-3) saga middleware 연결하기

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Head from "next/dist/next-server/lib/head";
 import PropTypes from 'prop-types';
 import withRedux from 'next-redux-wrapper';
-import AppLayout from "../components/AppLayout";
 import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+
+import AppLayout from "../components/AppLayout";
 import reducer  from '../reducers';
+import rootSaga from '../sagas';
+
+
 // import {initialState} from "../reducers/post";
 
 // Component는 next에서 넣어주는 props이다.
@@ -30,22 +35,37 @@ NodeBird.propTypes = {
 
 // nodeBird 컴포넌트에 props로 store를 넣어주는 역활을 함.
 // state와 reducer가 합쳐져 있는게 store라고 생각하면 편하다.
-export default withRedux((initialState, options) => {
-  // middleware는 redux에 없는 기능들을 추가 할때 주로 사용된다.
-  const middlewares = [];
-  
-  // enhancer는 middleware를 강화한다는 의미이다.
-  // applyMiddleware를 통해 middleware들을 적용한 뒤,
-  // compose를 통해 middleware들을 합성할 수 있다.
-  const enhancer = compose(applyMiddleware(...middlewares),
-    // 아래 부분은 REDUX DEV 공식사이트에서 가져온 내용으로, devtools를 다운받으면 __REDUX_DEVTOOLS_EXTENTION__함수가 생긴다.
-    // 이 함수를 사용하여 middleware를 생성하고 합성하는 코드이다.
-    // typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : (f) => f,
-    !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : (f) => f,
-    );
-  
+const configureStore = (initialState, options) => {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware]; // middleware는 redux에 없는 기능들을 추가 할때 주로 사용된다.
+
+  /**
+   * enhancer
+   * enhancer는 middleware를 강화한다는 의미이다.
+   * applyMiddleware를 통해 middleware들을 적용한 뒤,
+   * compose를 통해 middleware들을 합성할 수 있다.
+   *
+   * windwo.REDUX_DEVTOOLS_EXTENSION_CONPOSE
+   * window.REDUX_DEVTOOLS... 이하 아래부분은 REDUX DEV 공식사이트에서 가져온 내용으로, devtools를 다운받으면 __REDUX_DEVTOOLS_EXTENTION__함수가 생긴다.
+   * 이 함수를 사용하여 middleware를 생성하고 합성하는 코드이다.
+   * typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : (f) => f,
+   *
+   * 추가적으로 production일때는 redux의 state가 노출되면 안되므로, production일때 redux devtools에 대한 접근을 막아준다.
+   */
+
+  const enhancer = process.env.NODE_ENV === 'production' ?
+    compose(applyMiddleware(...middlewares))
+    :
+    compose(applyMiddleware(...middlewares),
+      !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : (f) => f,
+    )
+  ;
+
   const store = createStore(reducer, initialState, enhancer); // 루트 reducer를 넣어준다.
-  
-  
+  sagaMiddleware.run(rootSaga);
+
   return store;
-})(NodeBird);
+};
+
+// NodeBird 컴포넌트의 props로 store를 연결해주는 역활을 한다.
+export default withRedux(configureStore)(NodeBird);
