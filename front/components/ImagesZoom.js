d47135c4099d4e379702c02fb618cd8befdee660
jungwoole91 <jungwoolee91@gmail.com>
2021-01-14 01:42:02 +0900

    7-15) styled-components

--- front/components/ImagesZoom.js
+++ front/components/ImagesZoom.js
@@ -2,18 +2,83 @@ import PropTypes from 'prop-types';
 import Slick from 'react-slick';
 import { useState } from 'react';
 import { Icon } from 'antd';
+import styled from 'styled-components';
+
+const Overlay = styled.div`
+  position: fixed;
+  z-index: 5000;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+const Header = styled.header`
+  height: 44px;
+  background: white;
+  position: relative;
+  padding: 0;
+  text-align: center;
+  
+  & > h1 {
+    margin: 0;
+    font-size: 17px;
+    color: #333;
+    line-height: 44px 
+  }
+`;
+
+const SlickWrapper = styled.div`
+  height: calc(100% - 44px);
+  background: #090909
+`;
+
+const CloseBtn = styled(Icon)` // Antd 디자인을 CSS 덮어 씌워서 사용
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 15px;
+  line-height: 14px;
+  cursor: pointer
+`;
+
+const Indicator = styled.div`
+  textAlign: center; 
+
+  & > div {
+    width: 75px;
+    height: 30px;
+    line-height: 30px;
+    border-radius: 15px;
+    background: #313131;
+    display: inline-block;
+    text-align: center;
+    color: white;
+    font-size: 15px
+  }
+`;
+
+const ImgWrapper = styled.div`
+  padding: 32px;
+  text-align: center;
+  
+  & > img {
+    margin: 0 auto;
+    maxHeight: 750
+  }
+`;
 
 const ImagesZoom = ({ images, onClose }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   return (
-    <div style={{ position: 'fixed', zIndex: 5000, top: 0, left: 0, right: 0, bottom: 0 }}>
-      <header style={{ height: 44, background: 'white', position: 'relative', padding: 0, textAlign: 'center' }}>
-        <h1 style={{ margin: 0, fontSize: '17px', color: '#333', lineHeight: '44px' }}>상세 이미지</h1>
-        <Icon type="close" onClick={onClose} style={{ position: 'absolute', right: 0, top: 0, padding: 15, lineHeight: '14px', cursor: 'pointer' }}></Icon>
-      </header>
-      <div style={{ height: 'calc(100% - 44px)', background: '#090909' }}>
-        <div style={{paddingTop: '100px'}}>
+    <Overlay>
+      <Header>
+        <h1>상세 이미지</h1>
+        <CloseBtn type="close" onClick={onClose} />
+      </Header>
+      <SlickWrapper>
+        <div >
           <Slick
             initialSlide={0} // 맨처음에 로딩할 슬라이드 번호
             afterChange={slide => setCurrentSlide(slide)} // slide 변화 이후
@@ -24,18 +89,20 @@ const ImagesZoom = ({ images, onClose }) => {
           >
             {images.map((v) => {
               return (
-                <div>
-                  <img src={`http://localhost:3065/${v.src}`} style={{ margin: '0 auto', maxHeight: 750 }}/>
-                </div>
+                <ImgWrapper>
+                  <img src={`http://localhost:3065/${v.src}`} />
+                </ImgWrapper>
               );
             })}
           </Slick>
-          <div style={{ textAlign: 'center' }}>
-            <div style={{ width: 75, height: 30, lineHeight: '30px', borderRadius: 15, background: '#313131', display: 'inline-block', textAlign: 'center', color: 'white', fontSize: '15px' }}>{currentSlide + 1} / {images.length}</div>
-          </div>
+          <Indicator>
+            <div>
+              {currentSlide + 1} / {images.length}
+            </div>
+          </Indicator>
         </div>
-      </div>
-    </div>
+      </SlickWrapper>
+    </Overlay>
   );
 };
 
