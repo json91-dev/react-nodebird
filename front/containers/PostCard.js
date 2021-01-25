c3bbaf9219a92c310c9555625e507b22a76816d2
jungwoole91 <jungwoolee91@gmail.com>
2021-01-25 23:07:49 +0900

    8-4) tree shaking 예제와 gzip

--- front/containers/PostCard.js
+++ front/containers/PostCard.js
@@ -19,6 +19,8 @@ import {
   FOLLOW_USER_REQUEST,
   UNFOLLOW_USER_REQUEST,
 } from "../reducers/user";
+import moment from 'moment';
+moment.locale('ko');
 
 const CardWrapper = styled.div`
   margin-bottom: 20px;
@@ -174,6 +176,7 @@ const PostCard = ({ post }) => {
                 title={post.Retweet.User.nickname}
                 description={<PostCardContent postData={post.Retweet.content} />} // a tag x -> Link
               />
+              {moment(post.createdAt).format('YYYY.MM.DD')}
             </Card>
           )
           : (
--- front/next.config.js
+++ front/next.config.js
@@ -1,4 +1,6 @@
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = withBundleAnalyzer({
   analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
@@ -15,13 +17,37 @@ module.exports = withBundleAnalyzer({
   },
   distDir: '.next',
   webpack(config) {
-    console.log('config', config); // 기본적인 next의 config 출력
-    console.log('rules', config.module.rules[0]);
     const prod = process.env.NODE_ENV === 'production';
+
+    const plugins = [
+      ...config.plugins,
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+    ];
+
+    if (prod) {
+      plugins.push(new CompressionPlugin()); // main.js => main.js.gz)
+    }
+
+    // console.log('config', config); // 기본적인 next의 config 출력
+    // console.log('rules', config.module.rules[0]);
     return {
       ...config,
       mode: prod ? 'production' : 'development',
-      devtool: prod ? 'hidden-source-map' : 'eval', // production일떄 sourcemap 제공
+      devtool: prod ? 'hidden-source-map' : 'eval', // production일때 sourcemap 제공
+      module: {
+        ...config.module,
+        rules: [
+          ...config.module.rules,
+          {
+            loader: 'webpack-ant-icon-loader',
+            enforce: 'pre',
+            include: [
+              require.resolve('@ant-design/icons/lib/dist'),
+            ],
+          },
+        ],
+      },
+      plugins,
     };
   },
 });
