7e0add26b459effb7850fb2e5ec3cec3f3cbde85
jungwoole91 <jungwoolee91@gmail.com>
2021-02-15 23:26:41 +0900

    8-15) 프론트, 백엔드 쿠키 통일하기

--- back/index.js
+++ back/index.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
 const dotenv = require('dotenv');
 const passport = require('passport');
+const hpp = require('hpp');
+const helmet = require('helmet');
 
 const passportConfig = require('./passport');
 const db = require('./models');
@@ -13,23 +15,36 @@ const postAPIRouter = require('./routes/post');
 const postsAPIRouter = require('./routes/posts');
 const hashtagAPIRouter = require('./routes/hashtag');
 
+const prod = process.env.NODE_ENV === 'production';
+
 dotenv.config();
 const app = express();
 db.sequelize.sync(); // 테이블을 알아서 생성해 줌
 passportConfig(); // 패스포트 전략을 중앙통제실로 연결
 
+if (prod) {
+  app.use(hpp());
+  app.use(helmet());
+  app.use(morgan('combined'));
+  app.use(cors({
+    origin: 'http://jw910911.online',
+    credentials: true,
+  }));
+} else {
+  app.use(morgan('dev'));
+  app.use(cors({
+    origin: true,
+    credentials: true,
+  })); // cors 문제 처리
+}
+
 // 요청이 들어왔을때 요청을 찍어주는 기능을 할 수 있게된다.
-app.use(morgan('dev'));
+
 // static middleware
 app.use('/', express.static('uploads'));
 // 아래 두줄이 있을때 req.body를 사용할 수 있게 된다.
 app.use(express.json()); // JSON 형식의 본문을 처리한다.
 app.use(express.urlencoded({ extended: true })); // Form으로 넘어온 데이터를 처리한다.
-app.use(cors({
-  // origin: 'http://localhost:3000',
-  origin: true,
-  credentials: true,
-})); // cors 문제 처리
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(expressSession({
   resave: false,
@@ -38,6 +53,7 @@ app.use(expressSession({
   cookie: {
     httpOnly: true,
     secure: false,
+    domain: prod && '.jw910911.online',
   },
   name: 'jungwoo',
 }));
@@ -55,6 +71,6 @@ app.use('/api/posts', postsAPIRouter);
 app.use('/api/hashtag', hashtagAPIRouter);
 
 // 로컬 호스트의 서버 실행
-app.listen(process.env.NODE_ENV === 'production' ? process.env.PORT : 3065, () => {
+app.listen(prod ? process.env.PORT : 3065, () => {
   console.log(`server is running on localhost: ${process.env.NODE_ENV === 'production' ? process.env.PORT : 3065}`);
 });
